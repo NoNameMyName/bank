e139fdc2602c33459fe493db7ed9bd05d82f9d96
NoNameMyName <romasmikow2013@gmail.com>
2022-03-20 21:05:38 +0200

    fill_db was modified

--- bank/utils/fill_db.py
+++ bank/utils/fill_db.py
@@ -16,12 +16,13 @@ from bank.utils.constants import (
 
 def create_accounts(accounts_amount=10):
     fake = Faker()
+    accounts = dict()
     for _ in range(accounts_amount):
         name = fake.name().split()[FIRST_NAME_INDEX]
         type_of_card = TYPE_CARD[randint(0, 1)]
         currency_type = CURRENCY_TYPE[randint(0, 2)]
         money = MONEY
-        accounts = BankAccount(owner=name, type_card=type_of_card, currency_type=currency_type, balance=money)
+        accounts = accounts.update(BankAccount(owner=name, type_card=type_of_card, currency_type=currency_type, balance=money))
     BankAccount.objects.bulk_create(accounts)
 
 
